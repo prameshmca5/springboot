53c0e5b5cdffbf9fcb7e750a71a7652f4c97e568
Ramesh <rameshkp2@gmail.com>
2020-04-21 23:45:50 +0800

    add new file in thant project

---
+++ src/main/java/com/ramesh/springreact/springreactportal/SpringreactportalApplication.java
@@ -0,0 +1,13 @@
+package com.ramesh.springreact.springreactportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramesh/springreact/springreactportal/SpringreactportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramesh.springreact.springreactportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringreactportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
